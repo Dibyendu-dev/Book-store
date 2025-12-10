efd7eeced773e21fd378f23526ecf4bd3f9c5cc0
ddas4548 <ddas4548@gmail.com>
2025-12-10 15:58:57 +0530

    refactor controller layer

--- src/main/java/com/example/BookStore/controller/BookController.java
+++ src/main/java/com/example/BookStore/controller/BookController.java
@@ -1,6 +1,6 @@
 package com.example.BookStore.controller;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
@@ -10,39 +10,50 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.BookStore.dto.BookDTO;
+import com.example.BookStore.service.BookService;
 
 @RestController
 @RequestMapping("/bookstore")
 public class BookController {
 
+    BookService bookservice;
+
+    public BookController(BookService bookService){
+        this.bookservice=bookService;
+    }
+
     @GetMapping("/{bookId}")
-    public ResponseEntity<String> getBook(@PathVariable String bookId){
-        return new ResponseEntity<>("Book Found" + bookId, HttpStatus.OK);
+    public ResponseEntity<BookDTO> getBook(@PathVariable String bookId){
+       BookDTO bookDTO = bookservice.getBook(bookId);
+        return new ResponseEntity<>(bookDTO, HttpStatus.OK);
     }
 
     @GetMapping("/")
-    public ResponseEntity<List<String>> getAllBooks(){
-        List<String> books = new ArrayList<>();
-        books.add("java");
-        books.add("python");
-        books.add("javaScript");
-        return new ResponseEntity<>(books, HttpStatus.OK);
+    public ResponseEntity<List<BookDTO>> getAllBooks(){
+        List<BookDTO> booksDTOList = bookservice.getAllBooks(); 
+        return new ResponseEntity<>(booksDTOList, HttpStatus.OK);
     }
 
     @PostMapping("/")
-     public ResponseEntity<String> createBooks(String book){
-        return new ResponseEntity<>("books created succesfully", HttpStatus.OK);
+     public ResponseEntity<BookDTO> createBooks(@RequestBody BookDTO bookDTO){
+        BookDTO bookDTO1 = bookservice.createBook(bookDTO);
+        return new ResponseEntity<>(bookDTO1, HttpStatus.OK);
      }
 
      @PutMapping("/")
-      public ResponseEntity<String> updateBook(String book){
-        return new ResponseEntity<>("books updated succesfully", HttpStatus.OK);
+      public ResponseEntity<BookDTO> updateBook(@RequestBody BookDTO bookDTO){
+        BookDTO bookDTO1 = bookservice.updateBookName(bookDTO);
+        return new ResponseEntity<>(bookDTO1, HttpStatus.OK);
      }
 
     @DeleteMapping("/{bookId}")
     public ResponseEntity<String> deleteBook(@PathVariable String bookId){
+        bookservice.deleteBookByBookId(bookId);
         return new ResponseEntity<>("Book deleted successfully" , HttpStatus.OK);
     }
 }
